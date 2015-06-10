7e93e92e54972792b57b2898ccff522f48457bc4
mino.lukac <mino.lukac@gmail.com>
2015-06-10 22:57:15 +0200

    Class constructor.

--- src/EntityWrapper/Node/ArticleWrapper.php
+++ src/EntityWrapper/Node/ArticleWrapper.php
@@ -13,5 +13,10 @@ use \EntityDrupalWrapper;
  * Wraps nodes of type article with additional functionality.
  */
 class ArticleWrapper extends EntityDrupalWrapper {
+  protected $nodeWrapper;
+
+  public function __construct($nid) {
+    $this->nodeWrapper = new EntityDrupalWrapper('node', $nid);
+  }
 
 }
