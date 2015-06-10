d0861d3ee062286bc3063b5e33df9081201ed90d
mino.lukac <mino.lukac@gmail.com>
2015-06-10 22:53:39 +0200

    Initial version of Article wrapper example.

---
+++ src/EntityWrapper/Node/ArticleWrapper.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Article wrapper class.
+ */
+
+namespace Drupal\entity_class_generator\EntityWrapper\Node;
+
+use \EntityDrupalWrapper;
+
+/**
+ * Wraps nodes of type article with additional functionality.
+ */
+class ArticleWrapper extends EntityDrupalWrapper {
+
+}
