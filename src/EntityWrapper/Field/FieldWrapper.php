cdc3f65a616052e798099cda6d7dd4d57555d106
mino.lukac <mino.lukac@gmail.com>
2015-06-12 00:12:05 +0200

    Issue #8 - Initial Field wrapper class structure.

---
+++ src/EntityWrapper/Field/FieldWrapper.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Field wrapper class.
+ */
+
+namespace Drupal\entity_api_wrapper\EntityWrapper\Field;
+
+/**
+ * Class FieldWrapper.
+ *
+ * @package Drupal\entity_api_wrapper\EntityWrapper\Field
+ */
+abstract class FieldWrapper {
+
+}
